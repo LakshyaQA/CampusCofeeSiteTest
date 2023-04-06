b27d1bb450e07330b48fcbd39618b2933b4347f7
LakshyaSharmaQA <lakshyasharmaqa@gmaiol.com>
2023-04-07 02:22:18 +0530

    1

--- src/main/java/utils/TestUtils.java
+++ src/main/java/utils/TestUtils.java
@@ -6,8 +6,6 @@ public class TestUtils {
 
     public static final long IMPLICIT_WAIT = 20;
     public static final long PAGE_LOAD_WAIT = 20;
-
-
     public static String getTextOfElement(WebElement element){//why did static expalnation
         return element.getText();
     }
